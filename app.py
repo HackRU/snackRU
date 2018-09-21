5a36fcc1890907508c1f9475e724a820c41caab4
Andrea-Wu <ayw19@rutgers.edu>
2018-09-21 23:20:37 +0000

    adding login

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
-
 #Route for handling login
 @app.route('/login', methods=['GET','POST'])
 def login():
     error = None
-    return "THis is a login page!!!!!"
+    return "login page!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+
