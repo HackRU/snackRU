56718604059d0740ae8c94ad9d156dd6152444dc
Andrea-Wu <ayw19@rutgers.edu>
2018-11-24 20:09:18 +0000

    forms ooos

---
+++ forms.py
@@ -0,0 +1,9 @@
+from wtforms import Form, StringField, PasswordField, SubmitField, validators
+from flask_wtf import FlaskForm 
+class loginForm(FlaskForm):
+    username = StringField(u'username', [validators.required(), validators.length(max=10)])
+    password =  PasswordField(u'password',[validators.required()])
+    submit = SubmitField(u'submit')
+
+    class Meta:
+        csrf = False
---
+++ user.py
@@ -0,0 +1,5 @@
+class user:
+    def __init__(self, username):
+        self.username = username
+
+    
