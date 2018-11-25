7ad594c1bb0877cdc05f681d15d5cd04074b0899
Mazaya Rahman <mazaya.rahman@gmail.com>
2018-11-25 02:55:38 +0000

    adding mongo

--- mongo.py
+++ mongo.py
@@ -4,3 +4,9 @@ class DbUser(Document):
     username = StringField(max_length=10, required=True)
     password = StringField(required=True) #this will be hashed
     email = EmailField(required=True) #idk how this works tho
+
+class DbFood(Document):
+    foodName = StringField(required = True)
+    quantity = StringField()
+    category = StringField()
+
